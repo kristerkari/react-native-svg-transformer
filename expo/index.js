d9dd19a2df7661b1a8e8360051c9b1083ebaddca
Jean-Baptiste LARRIVIERE <76693792+jblarriviere@users.noreply.github.com>
2025-04-29 11:29:37 +0200

    fix: dont resolve rn transformer in expo projects

--- expo/index.js
+++ expo/index.js
@@ -1,3 +1,3 @@
-const { createTransformer, expoTransformer } = require("../index.js");
+const { createTransformer, getExpoTransformer } = require("../index.js");
 
-module.exports.transform = createTransformer(expoTransformer);
+module.exports.transform = createTransformer(getExpoTransformer());
