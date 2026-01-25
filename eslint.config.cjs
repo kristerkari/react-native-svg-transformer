d41b42e4d3c3eb2be4db0671302943fbf1cfcee5
Krister Kari <musky_heating0p@icloud.com>
2026-01-25 11:59:14 -0300

    Update ESLint

---
+++ eslint.config.cjs
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "script"
+    },
+    rules: {
+      "eqeqeq": ["error", "always", { "null": "ignore" }],
+      "indent": ["error", 2],
+      "quotes": ["error", "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
+      "semi": ["error", "always"],
+      "no-console": "warn",
+      "no-debugger": "error"
+    }
+  }
+];
